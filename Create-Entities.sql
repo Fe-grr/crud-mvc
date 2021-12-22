8edb78fa17fd6d132b18e33a224221931507203f
Felipe Gabriel <liperesende1@hotmail.com>
2021-12-21 23:22:06 -0300

    script sql

---
+++ Create-Entities.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Categorias
+(
+	CategoriaId INT IDENTITY(1,1) PRIMARY KEY,
+	Nome VARCHAR(255),
+	CriadoEm DATETIME,
+	AtualizadoEm DATETIME
+)
+
+CREATE TABLE Produtos
+(
+	ProdutoId INT IDENTITY(1,1) PRIMARY KEY,
+	Nome VARCHAR(255),
+	Preco DECIMAL,
+	Descricao VARCHAR(MAX),
+	CriadoEm DATETIME,
+	AtualizadoEm DATETIME,
+	CategoriaId INT FOREIGN KEY REFERENCES Categorias(CategoriaId)
+)
