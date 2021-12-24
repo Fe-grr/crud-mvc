49bcd5ee8dc2a95ff0993c9fb062cd3cb43b40fd
Felipinho <liperesende1@hotmail.com>
2021-12-24 14:30:46 -0300

    Corrigindo readme e arquivo sql

--- Create-Entities.sql
+++ Create-Entities.sql
@@ -1,22 +1,25 @@
---Executar script a script de cima a baixo
+--Executar script a script
 
 CREATE DATABASE Citel;
 
-CREATE TABLE Categorias
+CREATE TABLE citel.Categorias
 (
-	CategoriaId INT IDENTITY(1,1) PRIMARY KEY,
+	CategoriaId INT not null AUTO_INCREMENT,
 	Nome VARCHAR(255),
-	CriadoEm DATETIME,
-	AtualizadoEm DATETIME
-)
-
-CREATE TABLE Produtos
-(
-	ProdutoId INT IDENTITY(1,1) PRIMARY KEY,
-	Nome VARCHAR(255),
-	Preco DECIMAL,
-	Descricao VARCHAR(MAX),
 	CriadoEm DATETIME,
 	AtualizadoEm DATETIME,
-	CategoriaId INT FOREIGN KEY REFERENCES Categorias(CategoriaId)
+	primary key (CategoriaId)
+)
+
+CREATE TABLE citel.Produtos
+(
+	ProdutoId INT not null AUTO_INCREMENT,
+	Nome VARCHAR(255),
+	Preco DECIMAL,
+	Descricao VARCHAR(3000),
+	CriadoEm DATETIME,
+	AtualizadoEm DATETIME,
+	CategoriaId INT,
+	primary key (ProdutoId),
+	foreign key (CategoriaId) references Categorias(CategoriaId)
 )
--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 # crud-mvc
 CRUD de produtos e categorias
-
-    Passo 1: Executar as queries do arquivo Create-Entities.sql
-
-    Passo 2: Abrir solução Citel.WebApi.sln dentro da pasta Citel.WebApi
-   
-    2.1: Abrir arquivo Web.config
     
-    2.2: Editar a connectionString (linha 19) com o seu servidor que usou para criar as entidades no banco de dados (ex.: connectionString="Data Source=SEUSERVIDOR;...)
- 
-    Passo 3: Executar o projeto Citel.WebApi
+    Passo 1: Abrir Xampp e executar o MySQL na portão padrão 3306
+    
+    Passo 2: Executar as queries do arquivo Create-Entities.sql
+
+    Passo 3: Executar o projeto Citel.WebApi 
 
     Passo 4: Abrir solução Citel.web.sln dentro da pasta Citel.web
 
